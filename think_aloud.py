20b08ec3c6ef0047bef1eab14448be7e91f9ff0d
Issam Arabi <issam@cs.toronto.edu>
2023-10-23 19:43:49 -0400

    add function for summarizing insights

--- think_aloud.py
+++ think_aloud.py
@@ -19,60 +19,6 @@ def think_aloud_minutes(transcription):
     }
 
 
-def abstract_summary_extraction(transcription):
-    response = openai.ChatCompletion.create(
-        model="gpt-4",
-        temperature=0,
-        messages=[
-            {
-                "role": "system",
-                "content": "You are a highly skilled AI trained in language comprehension and summarization. I would like you to read the following transcription from a usability test of a website and summarize it into a concise abstract paragraph. Aim to retain the most important points, providing a coherent and readable summary that could help a person understand the main points of the discussion without needing to read the entire text. Please avoid unnecessary details or tangential points."
-            },
-            {
-                "role": "user",
-                "content": transcription
-            }
-        ]
-    )
-    return response['choices'][0]['message']['content']
-
-
-def key_points_extraction(transcription):
-    response = openai.ChatCompletion.create(
-        model="gpt-4",
-        temperature=0,
-        messages=[
-            {
-                "role": "system",
-                "content": "You are a proficient AI with a specialty in distilling information into key points. Based on the following transcription from a usability test of a website, identify and list the main points that were discussed or brought up. These should be the most important ideas, findings, or topics that are crucial to the essence of the discussion. Your goal is to provide a list that someone could read to quickly understand what was talked about."
-            },
-            {
-                "role": "user",
-                "content": transcription
-            }
-        ]
-    )
-    return response['choices'][0]['message']['content']
-
-
-def sentiment_analysis(transcription):
-    response = openai.ChatCompletion.create(
-        model="gpt-4",
-        temperature=0,
-        messages=[
-            {
-                "role": "system",
-                "content": "As an AI with expertise in language and emotion analysis, your task is to analyze the sentiment of the following transcription from a usability test of a website. Please consider the overall tone of the discussion, the emotion conveyed by the language used, and the context in which words and phrases are used. Indicate whether the sentiment is generally positive, negative, or neutral, and provide brief explanations for your analysis where possible."
-            },
-            {
-                "role": "user",
-                "content": transcription
-            }
-        ]
-    )
-    return response['choices'][0]['message']['content']
-
-
 def issues_extraction(transcription):
     response = openai.ChatCompletion.create(
         model="gpt-4",
@@ -89,6 +35,7 @@ def issues_extraction(transcription):
         ]
     )
     return response['choices'][0]['message']['content']
+
 
 def action_extraction(issues, transcription):
     response = openai.ChatCompletion.create(
@@ -109,3 +56,99 @@ def action_extraction(issues, transcription):
         ]
     )
     return response['choices'][0]['message']['content']
+
+
+def format_transcripts(transcripts):
+    """
+    Converts a list of transcripts into a readable string format for LLM processing.
+    """
+    formatted_transcripts = []
+    for idx, transcript in enumerate(transcripts):
+        formatted_transcript = f"Study {idx+1}:\n"
+        formatted_transcript += f"Usability Issues: {transcript['usability_issues']}\n"
+        formatted_transcript += f"Actionable Insights: {transcript['actionable_insights']}\n\n"
+        formatted_transcripts.append(formatted_transcript)
+
+    return "\n".join(formatted_transcripts)
+
+def summarize_insights(transcripts):
+    """
+    transcripts: list of transcripts (combined output of multiple calls to think_aloud_minutes())
+    """
+    
+    # Format transcripts for LLM
+    all_transcripts = format_transcripts(transcripts)
+
+    # Generate a summary based on all transcripts
+    response = openai.Completion.create(
+        model="gpt-4",
+        temperature=0,
+        messages=[
+            {
+                "role": "system",
+                "content": "You are given a collection of usability studies for a website. Each study contains a list of usability issues identified from a think-aloud session and actionable insights recommended for addressing those issues. Based on this data, please provide a summarized list of key insights and common themes observed across all studies."
+            },
+            {
+                "role": "user",
+                "content": all_transcripts
+            },
+        ],
+    )
+
+    return response['choices'][0]['message']['content']
+
+
+
+
+# def abstract_summary_extraction(transcription):
+#     response = openai.ChatCompletion.create(
+#         model="gpt-4",
+#         temperature=0,
+#         messages=[
+#             {
+#                 "role": "system",
+#                 "content": "You are a highly skilled AI trained in language comprehension and summarization. I would like you to read the following transcription from a usability test of a website and summarize it into a concise abstract paragraph. Aim to retain the most important points, providing a coherent and readable summary that could help a person understand the main points of the discussion without needing to read the entire text. Please avoid unnecessary details or tangential points."
+#             },
+#             {
+#                 "role": "user",
+#                 "content": transcription
+#             }
+#         ]
+#     )
+#     return response['choices'][0]['message']['content']
+
+
+# def key_points_extraction(transcription):
+#     response = openai.ChatCompletion.create(
+#         model="gpt-4",
+#         temperature=0,
+#         messages=[
+#             {
+#                 "role": "system",
+#                 "content": "You are a proficient AI with a specialty in distilling information into key points. Based on the following transcription from a usability test of a website, identify and list the main points that were discussed or brought up. These should be the most important ideas, findings, or topics that are crucial to the essence of the discussion. Your goal is to provide a list that someone could read to quickly understand what was talked about."
+#             },
+#             {
+#                 "role": "user",
+#                 "content": transcription
+#             }
+#         ]
+#     )
+#     return response['choices'][0]['message']['content']
+
+
+# def sentiment_analysis(transcription):
+#     response = openai.ChatCompletion.create(
+#         model="gpt-4",
+#         temperature=0,
+#         messages=[
+#             {
+#                 "role": "system",
+#                 "content": "As an AI with expertise in language and emotion analysis, your task is to analyze the sentiment of the following transcription from a usability test of a website. Please consider the overall tone of the discussion, the emotion conveyed by the language used, and the context in which words and phrases are used. Indicate whether the sentiment is generally positive, negative, or neutral, and provide brief explanations for your analysis where possible."
+#             },
+#             {
+#                 "role": "user",
+#                 "content": transcription
+#             }
+#         ]
+#     )
+#     return response['choices'][0]['message']['content']
